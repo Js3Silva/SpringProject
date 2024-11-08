23b23013f588aed2e3db57aaf12001cf3ae44d60
Jonathan Sena da Silva <jonathansilvagw@gmail.com>
2024-11-08 18:06:34 -0300

    correção do model para o uso do add(Link)

--- SpringApplication/springboot/src/main/java/com/example/springboot/models/ProductModel.java
+++ SpringApplication/springboot/src/main/java/com/example/springboot/models/ProductModel.java
@@ -5,10 +5,11 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 import jakarta.persistence.*;
+import org.springframework.hateoas.RepresentationModel;
 
 @Entity
 @Table(name= "TB_PRODUCTS")
-public class ProductModel implements Serializable{
+public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
